7f3fda3af65ebd79cbf204ceb60885f188304e39
Lukas Hermann <lukas.hermann@gmx.de>
2017-08-31 17:49:16 +0200

    error in timestamp

--- utils/options.py
+++ utils/options.py
@@ -38,7 +38,7 @@ class Params(object):   # NOTE: shared across all modules
 
         # training signature
         self.machine     = "lukas_aiscpu3"  # "machine_id"
-        self.timestamp   = "17083102"   # "yymmdd##"
+        self.timestamp   = "17083103"   # "yymmdd##"
         # training configuration
         self.mode        = 1            # 1(train) | 2(test model_file)
         self.config      = 14
